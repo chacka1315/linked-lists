8f4cb4ba10a988e0c7e70550f46930fe30ff7326
chacka1315 <chacka131@gmail.com>
2025-08-22 18:02:09 +0000

    I created class LinkedList for instacing a linked list and class Node for new node. I also wrote the method that append a new nodeon the list.

---
+++ linkedList.js
@@ -0,0 +1,16 @@
+import { Node } from "./node.js";
+export class LinkedList {
+  constructor() {}
+
+  append(value) {
+    if (!Object.keys(this).length) {
+      this.head = new Node(value);
+    } else {
+      let lastNode = this.head;
+      while (lastNode.nextNode !== null) {
+        lastNode = lastNode.nextNode;
+      }
+      lastNode.nextNode = new Node(value);
+    }
+  }
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+import { LinkedList } from "./linkedList.js";
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+
+console.log(list);

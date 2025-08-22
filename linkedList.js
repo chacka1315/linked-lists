32870c74bb3255ed80188a9d9e20d2090d8c2943
chacka1315 <chacka131@gmail.com>
2025-08-22 19:06:05 +0000

    i add a condition to create directly the node as the head when we prepend on empty list

--- linkedList.js
+++ linkedList.js
@@ -5,16 +5,20 @@ export class LinkedList {
   append(value) {
     if (!Object.keys(this).length) {
       this.head = new Node(value);
-    } else {
-      let lastNode = this.head;
-      while (lastNode.nextNode !== null) {
-        lastNode = lastNode.nextNode;
-      }
-      lastNode.nextNode = new Node(value);
+      return;
     }
+    let lastNode = this.head;
+    while (lastNode.nextNode !== null) {
+      lastNode = lastNode.nextNode;
+    }
+    lastNode.nextNode = new Node(value);
   }
 
   prepend(value) {
+    if (!Object.keys(this).length) {
+      this.head = new Node(value);
+      return;
+    }
     const currentHead = this.head;
     this.head = new Node(value);
     this.head.nextNode = currentHead;

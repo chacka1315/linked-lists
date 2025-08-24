848e3b02db98296f0c75ac5655707bc067a792ed
chacka1315 <chacka131@gmail.com>
2025-08-24 18:53:34 +0000

    I add more Methods test in main.js to test defferent linked list methods

--- main.js
+++ main.js
@@ -10,3 +10,7 @@ list.append("turtle");
 
 console.log(list.toString());
 // Expected output : ( dog ) -> ( cat ) -> ( parrot ) -> ( hamster ) -> ( snake ) -> ( turtle ) -> null
+console.log(list.contains("turtle")); //true
+console.log(list.find("turtle")); //5
+console.log(list.removeAt(5)); //true
+console.log(list.contains("turtle")); // false
